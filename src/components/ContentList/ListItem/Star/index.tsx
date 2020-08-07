1c34e6b65219387ab79fb826f1ef5c6db6d13106
SeekingLight233 <690163223@qq.com>
2020-08-07 13:03:49 +0800

    feat: 抽离星星渲染组件

---
+++ src/components/ContentList/ListItem/Star/index.tsx
@@ -0,0 +1,47 @@
+/**
+ * @description 主页上的头部组件
+ */
+import React from 'react';
+import './Star.scss';
+
+interface IProps {
+  starScore: number;
+}
+
+const Star: React.FC<IProps> = (props) => {
+  const { starScore } = props;
+  /**
+   * @description 渲染那几颗小星星~~
+   */
+  const renderStar = () => {
+    const stars = [];
+    const int_part = Math.floor(starScore);
+    const float_part = (starScore - int_part).toFixed(1);
+    console.log(`整数部分是${int_part}  小数部分${float_part}`);
+    const scoreArr = [];
+    for (let i = 0; i < 5; i++) {
+      if (i < int_part) {
+        scoreArr.push(1);
+      } else if (i === int_part) {
+        scoreArr.push(float_part);
+      } else {
+        scoreArr.push(0);
+      }
+    }
+    // [1, 1, 1, 1, "0.6"]
+
+    for (let i = 0; i < scoreArr.length; i++) {
+      if (scoreArr[i] === 1) {
+        stars.push(<i className='yellow-star' key={i}></i>);
+      } else if (scoreArr[i] < 1) {
+        stars.push(<i className='yellow-star half-star' key={i}></i>);
+      } else {
+        stars.push(<i className='yellow-star gray-star' key={i}></i>);
+      }
+    }
+    return stars;
+  };
+  return <span className='star clearfix'>{renderStar()}</span>;
+};
+
+export default Star;
--- src/components/ContentList/ListItem/index.tsx
+++ src/components/ContentList/ListItem/index.tsx
@@ -1,10 +1,11 @@
 /**
  * @description 单个商家列表
  */
-import React, { useState } from "react";
-import "./ListItem.scss";
-import { Item } from "../../../Models/contentList";
-import classNames from "classnames";
+import React, { useState } from 'react';
+import './ListItem.scss';
+import { Item } from '../../../Models/contentList';
+import classNames from 'classnames';
+import Star from './Star';
 
 const ListItem: React.FC<Item> = (props) => {
   const {
@@ -31,9 +32,9 @@ const ListItem: React.FC<Item> = (props) => {
   const renderDiscount = () => {
     return discounts2.map((item, index) => {
       return (
-        <span key={index} className="discount-wrap">
-          <div className="discount-wrap-item">
-            <img src={item.iconUrl} alt="" />
+        <span key={index} className='discount-wrap'>
+          <div className='discount-wrap-item'>
+            <img src={item.iconUrl} alt='' />
             <span>{item.info}</span>
           </div>
         </span>
@@ -42,7 +43,7 @@ const ListItem: React.FC<Item> = (props) => {
   };
 
   const renderKm = () => {
-    return <>{(distance as number) / 1000 + "km"}</>;
+    return <>{(distance as number) / 1000 + 'km'}</>;
   };
   /**
    * @description 渲染那几颗小星星~~
@@ -52,9 +53,9 @@ const ListItem: React.FC<Item> = (props) => {
     const score = Math.ceil(starScore);
     for (let i = 0; i <= 5; i++) {
       if (i < score) {
-        stars.push(<i className="star" key={i}></i>);
+        stars.push(<i className='star' key={i}></i>);
       } else {
-        stars.push(<i className="gray-star" key={i}></i>);
+        stars.push(<i className='gray-star' key={i}></i>);
       }
     }
     return stars;
@@ -66,38 +67,39 @@ const ListItem: React.FC<Item> = (props) => {
     setDiscount(!discount);
   };
   return (
-    <div className="list-item">
-      <div className="left-item">
-        <img className="item-img" src={picUrl} alt="" />
+    <div className='list-item'>
+      <div className='left-item'>
+        <img className='item-img' src={picUrl} alt='' />
       </div>
-      <div className="right-item">
-        <div className="shop-name">{shopName}</div>
-        <div className="item-desc ">
-          <span className="item-sales">
-            {renderStar()}
-            <span className="star-score">{starScore}</span>
-            {"月售" + monthSalesTip}
+      <div className='right-item'>
+        <div className='shop-name'>{shopName}</div>
+        <div className='item-desc '>
+          <span className='item-sales'>
+            <Star starScore={starScore}></Star>
+            <span className='star-score'>
+              {starScore} {'月售' + monthSalesTip}
+            </span>
           </span>
-          <span className="item-delivery">
+          <span className='item-delivery'>
             <span>{deliveryTimeTip}分钟</span>
-            <span className="item-distance">
-              {distance > 1000 ? renderKm() : distance + "m"}
+            <span className='item-distance'>
+              {distance > 1000 ? renderKm() : distance + 'm'}
             </span>
           </span>
         </div>
-        <div className="item-price">
-          <span className="item-price-detail">{minPriceTip}</span>
-          <span className="item-price-detail">{shippingFeeTip}</span>
-          <span className="item-price-detail">{averagePriceTip}</span>
-          {deliveryType === 1 ? <span className="meituan-tag"></span> : null}
+        <div className='item-price'>
+          <span className='item-price-detail'>{minPriceTip}</span>
+          <span className='item-price-detail'>{shippingFeeTip}</span>
+          <span className='item-price-detail'>{averagePriceTip}</span>
+          {deliveryType === 1 ? <span className='meituan-tag'></span> : null}
         </div>
-        <div className="item-recommend">
+        <div className='item-recommend'>
           {recommendInfo ? <span>大众点评高分店铺</span> : null}
         </div>
 
         {discounts2 ? (
           <div
-            className={classNames("discount-content", { toggle: discount })}
+            className={classNames('discount-content', { toggle: discount })}
             onClick={() => {
               if (discounts2.length >= 3) {
                 toggleDiscounts();
@@ -105,9 +107,9 @@ const ListItem: React.FC<Item> = (props) => {
             }}
           >
             <span
-              className={classNames("discount-arrow-up", {
-                "arrow-down": discount,
-                "hidden-arrow": discounts2.length < 3,
+              className={classNames('discount-arrow-up', {
+                'arrow-down': discount,
+                'hidden-arrow': discounts2.length < 3,
               })}
             ></span>
             {renderDiscount()}
