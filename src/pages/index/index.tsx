5d805d0771129931a3e09ef95f5ebac513bc940b
SeekingLight233 <690163223@qq.com>
2020-08-22 19:27:10 +0800

    refactor: 尝试使用 react-snap 优化首屏渲染速度

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -4,9 +4,19 @@ import { Provider } from 'react-redux';
 import store from '../../Models/dva';
 import Router from './Router/index';
 
-ReactDom.render(
-  <Provider store={store}>
-    <Router></Router>
-  </Provider>,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById('root');
+if (rootElement.hasChildNodes()) {
+  ReactDom.hydrate(
+    <Provider store={store}>
+      <Router></Router>
+    </Provider>,
+    rootElement
+  );
+} else {
+  ReactDom.render(
+    <Provider store={store}>
+      <Router></Router>
+    </Provider>,
+    rootElement
+  );
+}
